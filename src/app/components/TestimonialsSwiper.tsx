b8e84c6551b45ab3c2fa319cb67ba7186a90f6bb
Sohyb01 <sohyb0155@gmail.com>
2023-07-24 14:03:20 +0300

    debugging quotes and apostrophes

--- src/app/components/TestimonialsSwiper.tsx
+++ src/app/components/TestimonialsSwiper.tsx
@@ -4,9 +4,9 @@ const TestimonialsSwiper = () => {
       {/* Individual Testimonial */}
       <div className="flex w-full max-w-[336px] flex-col items-center gap-4 rounded-[16px] border-2 border-l-8 border-solid border-pes_lightgray border-l-pes_lightblue px-8 py-4 shadow-xl lg:max-w-[484px]">
         <div className="text-left text-sm text-pes_darkgray lg:text-base">
-          &quot;Thank you so much for your help. It&apos;s exactly what
-          I&apos;ve been looking for. You won&apos;t regret it. It really saves
-          me time and effort. PES is exactly what I have been looking for.&quot;
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi.
         </div>
         <div className="flex w-full justify-between">
           <h3 className="text-base lg:text-lg">Dr/ Folan Elfolany</h3>
@@ -50,9 +50,9 @@ const TestimonialsSwiper = () => {
       </div>
       <div className="hidden w-full max-w-[336px] flex-col items-center gap-4 rounded-[16px] border-2 border-l-8 border-solid border-pes_lightgray border-l-pes_lightblue px-8 py-4 shadow-xl md:flex lg:max-w-[484px]">
         <div className="text-left text-sm text-pes_darkgray lg:text-base">
-          &quot;Thank you so much for your help. It&apos;s exactly what
-          I&apos;ve been looking for. You won&apos;t regret it. It really saves
-          me time and effort. PES is exactly what I have been looking for.&quot;
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi.
         </div>
         <div className="flex w-full justify-between">
           <h3 className="text-base lg:text-lg">Dr/ Folan Elfolany</h3>
