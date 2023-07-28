b0e242c2e94fdfd0631dd5b020a020d6bd404ef0
Sohyb01 <sohyb0155@gmail.com>
2023-07-28 20:14:05 +0300

    playing with formtest page, rendering data

--- src/app/formtest/page.tsx
+++ src/app/formtest/page.tsx
@@ -1,27 +1,37 @@
 // import { NextRequest, NextResponse } from "next/server";
-// import prisma from "@/app/lib/prisma";
 
-// eslint-disable-next-line @next/next/no-async-client-component
+import { NextResponse } from "next/server";
+import { Key } from "react";
+
+// import prisma from "@/app/lib/prisma";
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
+
+async function getUsers() {
+  // Prisma queries here
+  const allUsers = await prisma.user.findMany();
+  // console.log({ allUsers });
+  return JSON.stringify({ allUsers });
+}
+
 export default async function Page() {
-  const request = await fetch(
-    "https://pes-website-nextjs-khs1s5l2h-sohyb01.vercel.app/api",
-    {
-      method: "GET",
-      cache: "no-cache",
-    }
-  );
-  if (!request.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    console.log("Didn't work");
-  } else {
-    const data = await request.json();
-    const usersData = data.allUsers;
-    console.log(usersData);
-  }
+  const request = await getUsers();
+  const useableData = JSON.parse(request);
+  const usersArray = useableData.allUsers;
 
   return (
     <>
       <h1>Form Test</h1>
+      <h1>Data is:</h1>
+      {usersArray.map((user: any) => {
+        return (
+          <div key={user.id}>
+            <h1>Name: {`${user.firstName} ${user.lastName}`}</h1>
+            <h2>Age: {user.age}</h2>
+          </div>
+        );
+      })}
     </>
   );
 }
