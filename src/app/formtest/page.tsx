3c5a5e86346593456aa18d143c5e1b43292328c4
Sohyb01 <sohyb0155@gmail.com>
2023-07-28 19:25:32 +0300

    Testing fetch

--- src/app/formtest/page.tsx
+++ src/app/formtest/page.tsx
@@ -3,10 +3,13 @@
 
 // eslint-disable-next-line @next/next/no-async-client-component
 export default async function Page() {
-  const request = await fetch("http://localhost:3000/api", {
-    method: "GET",
-    cache: "no-cache",
-  });
+  const request = await fetch(
+    "https://pes-website-nextjs-khs1s5l2h-sohyb01.vercel.app/api",
+    {
+      method: "GET",
+      cache: "no-cache",
+    }
+  );
   const data = await request.json();
   const usersData = data.allUsers;
   console.log(usersData);
